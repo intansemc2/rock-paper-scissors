eb666cbc80f66bee32dcc6af28e782e7062294fa
intansemc2 <intansemc2@gmail.com>
2020-09-29 04:09:20 +0700

    update app function

--- app.js
+++ app.js
@@ -6,30 +6,43 @@ let userScore = 0;
 let compScore = 0;
 
 //Cast the DOOM
-let userScore_span = document.querySelector('#user-score');
-let compScore_span = document.querySelector('#comp-score');
-let scoreBoard_div = document.querySelector('.score-board');
-let result_div = document.querySelector('.result');
+const userScore_span = document.querySelector('#user-score');
+const compScore_span = document.querySelector('#comp-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result > p');
 
-let rock_div = document.querySelector('#rock');
-let paper_div = document.querySelector('#paper');
-let scissors_div = document.querySelector('#scissors');
+const rock_div = document.querySelector('#rock');
+const paper_div = document.querySelector('#paper');
+const scissors_div = document.querySelector('#scissors');
 
 //Functions
 function getCompChoice() {
     return Math.floor(Math.random() * CHOICES.length);
 }
 
-function userWin() {
-    //
+function updateScoreBoard() {
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
 }
 
-function userLose() {
-    //
+function updateResult(userChoiceIndex, compChoiceIndex, result) {
+    result_div.innerHTML = `user<sub>${CHOICES[userChoiceIndex]}</sub> ${result} comp<sub>${CHOICES[compChoiceIndex]}</sub>`;
 }
 
-function draw() {
-    //
+function userWin(userChoiceIndex, compChoiceIndex) {
+    userScore += 1;
+    updateScoreBoard();
+    updateResult(userChoiceIndex, compChoiceIndex, 'beat');
+}
+
+function userLose(userChoiceIndex, compChoiceIndex) {
+    compScore += 1;
+    updateScoreBoard();
+    updateResult(userChoiceIndex, compChoiceIndex, 'lose');
+}
+
+function draw(userChoiceIndex, compChoiceIndex) {
+    updateResult(userChoiceIndex, compChoiceIndex, 'draw');
 }
 
 function game(userChoiceIndex) {
@@ -37,29 +50,29 @@ function game(userChoiceIndex) {
 
     //user the same as comp, draw
     if (userChoiceIndex == compChoiceIndex) {
-        draw();
+        draw(userChoiceIndex, compChoiceIndex);
     }
 
     //user win
     else if ((userChoiceIndex == 0 && compChoiceIndex == 2) || (userChoiceIndex == 1 && compChoiceIndex == 0) || (userChoiceIndex == 2 && compChoiceIndex == 1)) {
-        userWin();
+        userWin(userChoiceIndex, compChoiceIndex);
     }
 
     //user lose
     else {
-        userLose();
+        userLose(userChoiceIndex, compChoiceIndex);
     }
 }
 
 //Add event listeners
 rock_div.addEventListener('click', function () {
-    game(1);
+    game(0);
 });
 
 paper_div.addEventListener('click', function () {
-    game(2);
+    game(1);
 });
 
 scissors_div.addEventListener('click', function () {
-    game(3);
+    game(2);
 });
